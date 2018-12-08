c09a65bace2741d731c5b465af160d71dcde67b7
OBlom <blom.ola@gmail.com>
2018-12-08 19:58:07 +0100

    day2

--- solve.py
+++ solve.py
@@ -39,6 +39,50 @@ class Day1(Day):
                     print(freq)
                     freq_set.add(freq)
         print("Day1 challenge2: {}".format(freq)) 
+
+class Day2(Day):
+    def __init__(self):
+        Day.__init__(self,2)
+
+    def solve1(self):
+        sum3letters=0
+        sum2letters=0
+        for line in self.data:
+            letters={}
+            threeLetters=0
+            twoLetters=0
+            for c in line:
+                letters.setdefault(c,0)
+                letters[c]=letters[c]+1
+            for letter,val in letters.items():
+                if val == 3:
+                    threeLetters=1
+                if val == 2:
+                    twoLetters=1
+            sum3letters = sum3letters + threeLetters
+            sum2letters = sum2letters + twoLetters
+
+        print("Day2 challenge1: {}".format(sum2letters*sum3letters))
+
+    def solve2(self):
+        match=None
+        for i in range(0,len( self.data)-1):      
+            for j in range(i+1,len(self.data)):
+                diff=0
+                for a,b in zip(self.data[i],self.data[j]):
+                    if a != b:
+                        diff = diff+1
+                if diff == 1:
+                    match=''
+                    for a,b in zip(self.data[i],self.data[j]):
+                        if a == b:
+                            match = match + a
+                    break
+                if match is not None:
+                    break
+
+        print("Day2 challenge2: {}".format(match if match else 'Oops'))
+    
 def main(args):
     
     parser = argparse.ArgumentParser(
